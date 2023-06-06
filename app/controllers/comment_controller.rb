b5c5366c2b801f113e5325c4f857ec2228b5f09c
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-06-06 15:48:31 +0300

    updated controller

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,36 +1,3 @@
-# class CommentsController < Sinatra::Base
-#     def create
-#       comment_params = params.require(:comment).permit(:content, :article_id)
-      
-#       comment = Comment.new(comment_params)
-            
-#       # Save the comment
-#       if comment.save
-#         render json: { message: 'Comment saved successfully!' }, status: :ok
-#       else
-#         render json: { error: 'Error saving comment' }, status: :unprocessable_entity
-#       end
-#     end
-#   end
-# class CommentsController < Sinatra::Base
-#     post '/comments' do
-#       comment = Comment.new(comment_params)
-  
-#       if comment.save
-#         redirect "/articles/#{comment.article_id}" 
-#       else
-#         flash[:error] = 'Error saving comment'
-#         redirect back 
-#       end
-#     end
-  
-#     private
-  
-#     def comment_params
-#       params.require(:comment).permit(:content, :user_id, :article_id)
-#     end
-#   end
-
 class CommentsController < Sinatra::Base
     post '/comments' do
       comment = Comment.new(comment_params)
@@ -53,14 +20,11 @@ class CommentsController < Sinatra::Base
   
     def current_user
         User.find(session[:user_id]) if session[:user_id]
-    end
-      
+    end      
   
     def current_article
         Article.find(params[:article_id]) if params[:article_id]
     end
       
 end
-  
-  
   
