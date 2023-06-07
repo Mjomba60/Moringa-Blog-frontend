a2b8b1083e1ea907a5f2cb2d72d0f721b1194db0
George Samuel <geosam2708@gmail.com>
2023-06-07 12:29:19 +0300

    updated columns for db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_06_144239) do
+ActiveRecord::Schema.define(version: 2023_06_07_071131) do
 
   create_table "articles", force: :cascade do |t|
     t.string "category"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2023_06_06_144239) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "interaction_type"
+    t.string "image_url"
   end
 
   create_table "comments", force: :cascade do |t|
