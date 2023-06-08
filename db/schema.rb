2ea85ce89174cefa4528a359e4cf7fe2e65c815a
fascilio <fascilior@gmail.com>
2023-06-08 10:09:45 +0300

    updated files

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,10 @@ ActiveRecord::Schema.define(version: 2023_06_07_071131) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "user_name"
     t.string "password"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
